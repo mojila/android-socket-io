3fdef92d05be277f50f1ee3b8c7b1e1e484bbb54
Mojila <ajilaksono@outlook.co.id>
2020-05-17 16:48:52 +0700

    remove unused import

--- app/src/main/java/com/example/socketioclientexample/MainActivity.java
+++ app/src/main/java/com/example/socketioclientexample/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
